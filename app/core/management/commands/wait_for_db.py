4298bd6a13a09c126579864d87cfab06673b09fb
Akshay Kaushik <akshay.kaushik9@gmail.com>
2024-01-31 22:24:21 +0530

    Configured docker-compose and checks to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
     help = 'Check if the database is up and running.'
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        self.stdout.write('Waiting for database...')
+        self.stdout.write(self.style.NOTICE('Waiting for database...'))
         db_up = False
         while db_up is False:
+            time.sleep(1)
             try:
                 self.check(databases=['default'])
                 db_up=True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
