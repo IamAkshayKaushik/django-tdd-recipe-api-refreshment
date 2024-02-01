91656b925d55ad73c4c3ed3db945782d19cae567
Akshay Kaushik <akshay.kaushik9@gmail.com>
2024-02-01 19:22:16 +0530

    Added custom User model to Django admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin for custom User model with no email field."""
+
+    # Define fields for user list page
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_superuser']
+
+    # Define fields for edit user page
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Personal info'),
+            {'fields': ('name',)}
+        ),
+        (
+            _( 'Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}
+        ),
+        (
+            _('Important dates'),
+            {'fields': ('last_login',)}
+        )
+    )
+
+    readonly_fields = ['last_login']
+
+    # Define fields for add user page
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')}
+        ),
+    )
+
+admin.site.register(models.User, UserAdmin)
